4f6e970e5f2ae2415ad1ba0ca73a45552116ddc7
rabiiwoof <niecheeel@gmail.com>
2020-05-13 21:43:39 +0800

    v2

--- DiceIt/ViewController.swift
+++ DiceIt/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     // Interface Builder Outlet
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
     let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
